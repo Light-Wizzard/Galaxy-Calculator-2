d256ee0db0bcfde661dfbf1083c74a44429cadff
Light-Wizzard <thelightwizzard@gmail.com>
2021-07-21 09:17:15 -0700

    Update

--- scripts/build_script.cmd
+++ scripts/build_script.cmd
@@ -1,26 +1,26 @@
-write-host "build_script Windows"
-#
-cd %APPVEYOR_BUILD_FOLDER%
-mkdir build
-cd build
-mkdir AppDir
-if %platform%==x86 qmake -v && qmake ..\%BIN_PRO_RES_NAME%.pro CONFIG+=%configuration% CONFIG+=c++11 DESTDIR=%cd% && mingw32-make && mingw32-make install INSTALL_ROOT=AppDir
-if %platform%==x64 qmake -v && qmake ..\%BIN_PRO_RES_NAME%.pro CONFIG+=%configuration% CONFIG+=c++11 DESTDIR=%cd% && mingw32-make && mingw32-make install INSTALL_ROOT=AppDir
-write-host "after_build Windows"
-dir
-copy "%APPVEYOR_BUILD_FOLDER%\build\%BIN_PRO_RES_NAME%.exe" "AppDir\%BIN_PRO_RES_NAME%-%MY_OS%-%CONFIGURATION%-%platform%.exe"
-windeployqt "AppDir/%BIN_PRO_RES_NAME%.exe" --verbose=2
-#for %%I in (%PRJLIBS%) do copy %PRJLIBDIR%\%%I AppDir\
-7z a -tzip "%BIN_PRO_RES_NAME%-%MY_OS%-%CONFIGURATION%-%platform%.zip" AppDir -r
-copy "%APPVEYOR_BUILD_FOLDER%\build\%BIN_PRO_RES_NAME%-%MY_OS%-%CONFIGURATION%-%platform%.zip" "%APPVEYOR_BUILD_FOLDER%\
-echo APPVEYOR_BUILD_FOLDER=%APPVEYOR_BUILD_FOLDER%
-#xcopy /s /e /f "%APPVEYOR_BUILD_FOLDER%\build\AppDir" "%APPVEYOR_BUILD_FOLDER%\%QIF_PACKAGE_URI%"
-#C:\Qt\QtIFW-3.1.1\bin\binarycreator.exe --offline-only -c "%APPVEYOR_BUILD_FOLDER%\config\config.xml" -p "%APPVEYOR_BUILD_FOLDER%\packages" "%BIN_PRO_RES_NAME%-Windows-Installer.exe"
-#7z a -tzip "%BIN_PRO_RES_NAME%-Windows-Installer.zip" "%BIN_PRO_RES_NAME%-Windows-Installer.exe"
-C:\Qt\Tools\QtInstallerFramework\binarycreator.exe --offline-only -c "%APPVEYOR_BUILD_FOLDER%\config\config.xml" -p "%APPVEYOR_BUILD_FOLDER%\packages" "%BIN_PRO_RES_NAME%-Windows-Installer.exe"
-copy *.zip %APPVEYOR_BUILD_FOLDER%
-copy *.exe %APPVEYOR_BUILD_FOLDER%
-cd %APPVEYOR_BUILD_FOLDER%
-echo Current Path is %cd%
-dir
+write-host "build_script Windows" 
+# 
+cd %APPVEYOR_BUILD_FOLDER% 
+mkdir build 
+cd build 
+mkdir AppDir 
+if %platform%==x86 qmake -v && qmake ..\%BIN_PRO_RES_NAME%.pro CONFIG+=%configuration% CONFIG+=c++11 DESTDIR=%cd% && mingw32-make && mingw32-make install INSTALL_ROOT=AppDir 
+if %platform%==x64 qmake -v && qmake ..\%BIN_PRO_RES_NAME%.pro CONFIG+=%configuration% CONFIG+=c++11 DESTDIR=%cd% && mingw32-make && mingw32-make install INSTALL_ROOT=AppDir 
+write-host "after_build Windows" 
+dir 
+copy "%APPVEYOR_BUILD_FOLDER%\build\%BIN_PRO_RES_NAME%.exe" "AppDir\%BIN_PRO_RES_NAME%-%MY_OS%-%CONFIGURATION%-%platform%.exe" 
+windeployqt "AppDir/%BIN_PRO_RES_NAME%.exe" --verbose=2 
+#for %%I in (%PRJLIBS%) do copy %PRJLIBDIR%\%%I AppDir\ 
+7z a -tzip "%BIN_PRO_RES_NAME%-%MY_OS%-%CONFIGURATION%-%platform%.zip" AppDir -r 
+copy "%APPVEYOR_BUILD_FOLDER%\build\%BIN_PRO_RES_NAME%-%MY_OS%-%CONFIGURATION%-%platform%.zip" "%APPVEYOR_BUILD_FOLDER%\ 
+echo APPVEYOR_BUILD_FOLDER=%APPVEYOR_BUILD_FOLDER% 
+#xcopy /s /e /f "%APPVEYOR_BUILD_FOLDER%\build\AppDir" "%APPVEYOR_BUILD_FOLDER%\%QIF_PACKAGE_URI%" 
+#C:\Qt\QtIFW-3.1.1\bin\binarycreator.exe --offline-only -c "%APPVEYOR_BUILD_FOLDER%\config\config.xml" -p "%APPVEYOR_BUILD_FOLDER%\packages" "%BIN_PRO_RES_NAME%-Windows-Installer.exe" 
+#7z a -tzip "%BIN_PRO_RES_NAME%-Windows-Installer.zip" "%BIN_PRO_RES_NAME%-Windows-Installer.exe" 
+C:\Qt\Tools\QtInstallerFramework\binarycreator.exe --offline-only -c "%APPVEYOR_BUILD_FOLDER%\config\config.xml" -p "%APPVEYOR_BUILD_FOLDER%\packages" "%BIN_PRO_RES_NAME%-Windows-Installer.exe" 
+copy *.zip %APPVEYOR_BUILD_FOLDER% 
+copy *.exe %APPVEYOR_BUILD_FOLDER% 
+cd %APPVEYOR_BUILD_FOLDER% 
+echo Current Path is %cd% 
+dir 
 write-host "Completed-Build Windows"
--- scripts/install.cmd
+++ scripts/install.cmd
@@ -1,10 +1,10 @@
-write-host "install Windows"
-#
-if %platform%==x86 set PATH=%PY_DIR_32%;%PY_DIR_32%\Scripts;%PATH%
-if %platform%==x86 set PATH=%MINGW_32%\bin;%QT5_32%\bin;%PATH%
-if %platform%==x64 set PATH=%PY_DIR_64%;%PY_DIR_64%\Scripts;%PATH%
-if %platform%==x64 set PATH=%MINGW_64%\bin;%QT5_64%\bin;%PATH%
-if %platform%==x86 call "%QT5_32%\bin\qtenv2.bat"
-if %platform%==x64 call "%QT5_64%\bin\qtenv2.bat"
-if %platform%==x86 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars32.bat"
+write-host "install Windows" 
+# 
+if %platform%==x86 set PATH=%PY_DIR_32%;%PY_DIR_32%\Scripts;%PATH% 
+if %platform%==x86 set PATH=%MINGW_32%\bin;%QT5_32%\bin;%PATH% 
+if %platform%==x64 set PATH=%PY_DIR_64%;%PY_DIR_64%\Scripts;%PATH% 
+if %platform%==x64 set PATH=%MINGW_64%\bin;%QT5_64%\bin;%PATH% 
+if %platform%==x86 call "%QT5_32%\bin\qtenv2.bat" 
+if %platform%==x64 call "%QT5_64%\bin\qtenv2.bat" 
+if %platform%==x86 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars32.bat" 
 if %platform%==x64 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
