3c5d65e8d0b180486302b141125cb7e227eda694
Light-Wizzard <thelightwizzard@gmail.com>
2021-08-22 09:37:49 -0700

    Update

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -9,8 +9,8 @@ If ($env:PLATFORM -eq "x64" -And $env:MY_COMPILER -eq "Qt") {
     $env:Path += ";C:\Python$env:MY_PYTHON_VER-x64"
     $env:Path += ";C:\MINGW\bin\"
     $env:MY_QT_BAT = "C:\Qt\$env:MY_QT_VERSION\$env:MY_QT_MINGW64\bin\qtenv2.bat"
-    #$env:MY_VS_BAT = "C:\PROGRA~2\MICROS~3\2019\COMMUN~1\VC\AUXILI~1\Build\vcvars64.bat"
-    $env:MY_VS_BAT = "C:\Program Files (x86)\Microsoft Visual Studio\$env:MY_VS_VERSION\Community\VC\Auxiliary\Build\vcvars64.bat"
+    $env:MY_VS_BAT = "C:\PROGRA~2\MICROS~3\2019\COMMUN~1\VC\AUXILI~1\Build\vcvars64.bat"
+    #$env:MY_VS_BAT = "C:\Program Files (x86)\Microsoft Visual Studio\$env:MY_VS_VERSION\Community\VC\Auxiliary\Build\vcvars64.bat"
     Invoke-Expression $env:MY_QT_BAT
     Invoke-Expression $env:MY_VS_BAT
     #iex $env:MY_VS_BAT
