b99ad5ce3d4b4a1bf5c7ca11bc7bc56f206c17c7
Light-Wizzard <thelightwizzard@gmail.com>
2021-08-22 10:31:56 -0700

    Update

--- scripts/build_script.ps1
+++ scripts/build_script.ps1
@@ -7,15 +7,11 @@ If ($env:PLATFORM -eq "x64" -And $env:MY_COMPILER -eq "Qt") {
     $env:INSTALL_ROOT = 'AppDir'
     $env:DESTDIR = 'AppDir'
     $env:BUILD_ROOT = "$env:APPVEYOR_BUILD_FOLDER\build"
-    #$env:MY_QMAKE = "qmake.exe -r -Wall -Wlogic -Wparser $env:APPVEYOR_BUILD_FOLDER\$env:MY_BIN_PRO_RES_NAME.pro -o AppDir CONFIG+=$env:CONFIGURATION CONFIG+=c++11 CONFIG+=x86_64 DESTDIR=AppDir $env:APPVEYOR_BUILD_FOLDER\build"
-    #$env:MY_QMAKE = "qmake.exe -r -Wall -Wlogic -Wparser $env:APPVEYOR_BUILD_FOLDER\$env:MY_BIN_PRO_RES_NAME.pro -o AppDir CONFIG+=$env:CONFIGURATION CONFIG+=c++11 CONFIG+=x86_64 DESTDIR=AppDir"
     $env:MY_QMAKE = "qmake.exe $env:APPVEYOR_BUILD_FOLDER\$env:MY_BIN_PRO_RES_NAME.pro CONFIG+=$env:CONFIGURATION CONFIG+=x86_64"
     Write-Host "qmake dbug: $env:MY_QMAKE"
     Invoke-Expression $env:MY_QMAKE
-    #cmd /c $env:MY_QMAKE
-    #Invoke-Expression ($env:MY_QMAKE)
     If ($?) {
-        Write-Host "build_script Windows QT x64 mingw32-make -j 2"
+        Write-Host "build_script Windows QT x64 mingw32-make"
         Invoke-Expression "mingw32-make.exe"
         If ($?) {
             Write-Host "build_script Windows QT x64 mingw32-make install"
@@ -31,11 +27,23 @@ ElseIf ($env:PLATFORM -eq "x86" -And $env:MY_COMPILER -eq "Qt") {
     Set-Location -Path $env:APPVEYOR_BUILD_FOLDER
     New-Item -Path 'build' -ItemType Directory
     Set-Location -Path build
-    $env:INSTALL_ROOT=AppDir
+    $env:INSTALL_ROOT = 'AppDir'
+    $env:DESTDIR = 'AppDir'
     $env:BUILD_ROOT = "$env:APPVEYOR_BUILD_FOLDER\build"
-    Invoke-Expression "qmake -r -Wall -Wlogic -Wparser -spec win32-g++ $env:APPVEYOR_BUILD_FOLDER\$env:MY_BIN_PRO_RES_NAME.pro -o AppDir CONFIG+=$env:CONFIGURATION CONFIG+=c++11 CONFIG+=x86 DESTDIR=AppDir $env:APPVEYOR_BUILD_FOLDER\build"
-    Invoke-Expression "mingw32-make -j 2"
-    Invoke-Expression "mingw32-make install INSTALL_ROOT=AppDir"
+    $env:MY_QMAKE = "qmake.exe $env:APPVEYOR_BUILD_FOLDER\$env:MY_BIN_PRO_RES_NAME.pro CONFIG+=$env:CONFIGURATION CONFIG+=x86"
+    Write-Host "qmake dbug: $env:MY_QMAKE"
+    Invoke-Expression $env:MY_QMAKE
+    If ($?) {
+        Write-Host "build_script Windows QT x64 mingw32-make"
+        Invoke-Expression "mingw32-make.exe"
+        If ($?) {
+            Write-Host "build_script Windows QT x64 mingw32-make install"
+            Invoke-Expression "mingw32-make.exe install"
+            If ($?) {
+                $env:MY_BUILD_GOOD = true
+            }
+        }
+    }
 }
 ElseIf ($env:PLATFORM -eq "x64" -And $env:MY_COMPILER -eq "Vs") {
     Write-Host "build_script Windows VS x64" -ForegroundColor DarkYellow
@@ -44,12 +52,19 @@ ElseIf ($env:PLATFORM -eq "x64" -And $env:MY_COMPILER -eq "Vs") {
     Set-Location -Path build
     $env:INSTALL_ROOT=AppDir
     $env:BUILD_ROOT = "$env:APPVEYOR_BUILD_FOLDER\build"
-    $env:MY_QT_MAKE="qmake -r -Wall -Wlogic -Wparser $env:APPVEYOR_BUILD_FOLDER%\$env:MY_BIN_PRO_RES_NAME.pro CONFIG+=$env:CONFIGURATION CONFIG+=c++11 CONFIG+=x86_64 DESTDIR=AppDir $env:APPVEYOR_BUILD_FOLDER\build"
+    $env:MY_QT_MAKE="qmake $env:APPVEYOR_BUILD_FOLDER%\$env:MY_BIN_PRO_RES_NAME.pro CONFIG+=$env:CONFIGURATION CONFIG+=x86_64"
     Invoke-Expression $env:MY_QT_MAKE
-    $env:MY_QT_MAKE="mmake"
-    Invoke-Expression $env:MY_QT_MAKE
-    $env:MY_QT_MAKE="mmake install INSTALL_ROOT=AppDir"
-    Invoke-Expression $env:MY_QT_MAKE
+    If ($?) {
+        Write-Host "build_script Windows VS x64 mmake"
+        Invoke-Expression "mmake"
+        If ($?) {
+            Write-Host "build_script Windows VS x64 mmake install INSTALL_ROOT=AppDir"
+            Invoke-Expression "mmake install INSTALL_ROOT=AppDir"
+            If ($?) {
+                $env:MY_BUILD_GOOD = true
+            }
+        }
+    }
 }
 ElseIf ($env:PLATFORM -eq "x86" -And $env:MY_COMPILER -eq "Vs") {
     Write-Host "build_script Windows VS x86" -ForegroundColor DarkMagenta
@@ -58,12 +73,19 @@ ElseIf ($env:PLATFORM -eq "x86" -And $env:MY_COMPILER -eq "Vs") {
     Set-Location -Path build
     $env:INSTALL_ROOT=AppDir
     $env:BUILD_ROOT = "$env:APPVEYOR_BUILD_FOLDER\build"
-    $env:MY_QT_MAKE="qmake -r -Wall -Wlogic -Wparser -spec win32-g++ $env:APPVEYOR_BUILD_FOLDER\$env:MY_BIN_PRO_RES_NAME.pro CONFIG+=$env:CONFIGURATION CONFIG+=c++11 CONFIG+=x86 DESTDIR=AppDir $env:APPVEYOR_BUILD_FOLDER\build"
+    $env:MY_QT_MAKE="qmake -spec win32-g++ $env:APPVEYOR_BUILD_FOLDER\$env:MY_BIN_PRO_RES_NAME.pro CONFIG+=$env:CONFIGURATION CONFIG+=x86"
     Invoke-Expression $env:MY_QT_MAKE
-    $env:MY_QT_MAKE="mmake"
-    Invoke-Expression $env:MY_QT_MAKE
-    $env:MY_QT_MAKE="mmake install INSTALL_ROOT=AppDir"
-    Invoke-Expression $env:MY_QT_MAKE
+    If ($?) {
+        Write-Host "build_script Windows VS x86 mmake"
+        Invoke-Expression "mmake"
+        If ($?) {
+            Write-Host "build_script Windows VS x86 mmake install INSTALL_ROOT=AppDir"
+            Invoke-Expression "mmake install INSTALL_ROOT=AppDir"
+            If ($?) {
+                $env:MY_BUILD_GOOD = true
+            }
+        }
+    }
 }
 
 If ($env:MY_BUILD_GOOD -eq "true") {
