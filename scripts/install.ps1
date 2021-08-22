77bc5a5f8bed478252c32ea57ba4465d9813e203
Light-Wizzard <thelightwizzard@gmail.com>
2021-08-21 18:11:41 -0700

    Update

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -9,8 +9,8 @@ If ($env:PLATFORM -eq "x64" -And $env:MY_COMPILER -eq "Qt") {
    $env:MY_VS_BAT = "C:\Program Files (x86)\Microsoft Visual Studio\$env:MY_VS_VERSION\Community\VC\Auxiliary\Build\vcvars64.bat"
    Invoke-Expression $env:MY_QT_BAT
    #Invoke-Expression $env:MY_VS_BAT
-   Invoke-Expression "C:\Program Files (x86)\Microsoft Visual Studio\$env:MY_VS_VERSION\Community\VC\Auxiliary\Build\vcvars64.bat"
-   #iex "C:\Program Files (x86)\Microsoft Visual Studio\$env:MY_VS_VERSION\Community\VC\Auxiliary\Build\vcvars64.bat"
+   #Invoke-Expression "C:\Program Files (x86)\Microsoft Visual Studio\$env:MY_VS_VERSION\Community\VC\Auxiliary\Build\vcvars64.bat"
+   iex "C:\Program Files (x86)\Microsoft Visual Studio\$env:MY_VS_VERSION\Community\VC\Auxiliary\Build\vcvars64.bat"
 }
 Else {
    Write-Host "install Windows QT x86" -ForegroundColor DarkGreen
