74c22125ba7a415bcea6a7d1035cb27a27152f93
Light-Wizzard <thelightwizzard@gmail.com>
2021-08-26 22:03:16 -0700

    Update

--- scripts/install_mac.sh
+++ scripts/install_mac.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Last Update: 25 Auguest 2021
+# Last Update: 26 Auguest 2021
 #
 # shell?check -x scripts/install.sh
 #
@@ -11,7 +11,8 @@ brew cleanup;
 brew config;
 brew doctor || true;
 brew install --cask cmake;
-brew install qt5 binutils create-dmg openssl@1.1 curl;
+brew install curl autoconf automake libtool pkg-config;
+brew install qt5 binutils create-dmg openssl@1.1;
 brew link qt5 --force;
 export PATH="$(brew --prefix qt5)/bin:$PATH";
 export CMAKE_PREFIX_PATH="$(brew --prefix qt5)";
@@ -19,6 +20,7 @@ if ! softwareupdate --all --install --force; then
     sudo rm -rf /Library/Developer/CommandLineTools;
     sudo xcode-select --install;
 fi
+xcode-select --install;
 #    curl -fsS -o scripts/QtInstallerFramework-macOS.dmg "https://download.qt.io/official_releases/qt-installer-framework/$MY_QT_IF_VERSION/QtInstallerFramework-macOS-x86_64-$MY_QT_IF_VERSION.dmg";
 #    # make it executable
 #    chmod +x scripts/QtInstallerFramework-macOS.dmg;
